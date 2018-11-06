ccda15e201eac3bb2ef0d7c38504a530b6137dc2
Richard Mangerie <rmange418@comcast.com>
2018-11-06 12:07:30 -0500

    Uploaded card images to Amazon S3 Management Console

--- lambdaFun/JavaScript/index.js
+++ lambdaFun/JavaScript/index.js
@@ -4,6 +4,34 @@ var startIntent = false;
 var PN_GLOBAL = "";
 var BeAnywhere_GLOBAL = "";
 
+//<a href="https://www.iconfinder.com/icons/309047/conference_group_people_users_icon" target="_blank">"Conference, group, people, users icon"</a> by <a href="https://www.iconfinder.com/visualpharm" target="_blank">Ivan Boyko</a> is licensed under <a href="http://creativecommons.org/licenses/by/3.0" target="_blank">CC BY 3.0</a>
+//"Conference, group, people, users icon" (https://www.iconfinder.com/icons/309047/conference_group_people_users_icon) by Ivan Boyko (https://www.iconfinder.com/visualpharm) is licensed under CC BY 3.0 (http://creativecommons.org/licenses/by/3.0)
+var conferenceImg = {
+    small: "https://s3.amazonaws.com/audio-conference/images/conferenceSmall.png",
+    large: "https://s3.amazonaws.com/audio-conference/images/conferenceLarge.png"
+};
+
+//<a href="https://www.iconfinder.com/icons/3324959/outgoing_phone_icon" target="_blank">"Outgoing, phone icon"</a> by <a href="https://www.iconfinder.com/colebemis" target="_blank">Cole Bemis</a> is licensed under <a href="http://creativecommons.org/licenses/by/3.0" target="_blank">CC BY 3.0</a>
+//"Outgoing, phone icon" (https://www.iconfinder.com/icons/3324959/outgoing_phone_icon) by Cole Bemis (https://www.iconfinder.com/colebemis) is licensed under CC BY 3.0 (http://creativecommons.org/licenses/by/3.0)
+var phoneStartImg = {
+    small: "https://s3.amazonaws.com/audio-conference/images/phoneStartSmall.png",
+    large: "https://s3.amazonaws.com/audio-conference/images/phoneStartLarge.png"
+};
+
+//<a href="https://www.iconfinder.com/icons/3324961/missed_phone_icon" target="_blank">"Missed, phone icon"</a> by <a href="https://www.iconfinder.com/colebemis" target="_blank">Cole Bemis</a> is licensed under <a href="http://creativecommons.org/licenses/by/3.0" target="_blank">CC BY 3.0</a>
+//"Missed, phone icon" (https://www.iconfinder.com/icons/3324961/missed_phone_icon) by Cole Bemis (https://www.iconfinder.com/colebemis) is licensed under CC BY 3.0 (http://creativecommons.org/licenses/by/3.0)
+var phoneStopImg = {
+    small: "https://s3.amazonaws.com/audio-conference/images/phoneStopSmall.png",
+    large: "https://s3.amazonaws.com/audio-conference/images/phoneStopLarge.png"
+};
+
+//<a href="https://www.iconfinder.com/icons/3324960/off_phone_icon" target="_blank">"Off, phone icon"</a> by <a href="https://www.iconfinder.com/colebemis" target="_blank">Cole Bemis</a> is licensed under <a href="http://creativecommons.org/licenses/by/3.0" target="_blank">CC BY 3.0</a>
+//"Off, phone icon" (https://www.iconfinder.com/icons/3324960/off_phone_icon) by Cole Bemis (https://www.iconfinder.com/colebemis) is licensed under CC BY 3.0 (http://creativecommons.org/licenses/by/3.0)
+var phoneErrorImg = {
+    small: "https://s3.amazonaws.com/audio-conference/images/phoneErrorSmall.png",
+    large: "https://s3.amazonaws.com/audio-conference/images/phoneErrorLarge.png"
+};
+
 //event = input JSON
 exports.handler = function(event,context) {
 
@@ -47,7 +75,7 @@ exports.handler = function(event,context) {
         context.fail("Exception: " + e);
     }
 
-}
+};
 
 function buildResponse(options) {
 
@@ -95,6 +123,13 @@ function buildResponse(options) {
                 smallImageUrl: options.imageUrl,
                 largeImageUrl: options.imageUrl
             };
+        } else if (options.imageObj) {
+            response.response.card.type = "Standard";
+            response.response.card.text = options.cardContent;
+            response.response.card.image = {
+                smallImageUrl: options.imageObj.small,
+                largeImageUrl: options.imageObj.large
+            };
         } else {
             response.response.card.content = options.cardContent;
         }
@@ -114,7 +149,7 @@ function handleLaunchRequest(context) {
     options.speechText = "Welcome to the Audio Conference skill.Using this skill, you can start an audio conference on a telephone number or a Be Anywhere device.You can say, for example, ask audio conference to start a conference on <say-as interpret-as=\"telephone\">2155551234</say-as>, or, ask audio conference to start a conference on My Cell.";
     options.cardContent = "Welcome to the Audio Conference skill.  Using this skill, you can start an audio conference on a telephone number or a Be Anywhere device.  You can say, for example, ask audio conference to start a conference on 2155551234, or, ask audio conference to start a conference on My Cell.";
     options.cardTitle = "Audio Conference";
-    options.imageUrl = "https://cdn.pixabay.com/photo/2017/09/28/22/14/speech-icon-2797263_960_720.png";
+    options.imageObj = conferenceImg;
     options.endSession = true;
 
     //Outputting the Launch JSON to the console
@@ -147,17 +182,17 @@ function handleStartIntent(request,context,session) {
             BeAnywhere_GLOBAL = BeAnywhere;
         }
 
-        options.imageUrl = "https://www.pngimg.com/uploads/phone/phone_PNG49047.png";
+        options.imageObj = phoneStartImg;
+        
+        options.cardTitle = "Audio Conference Start";
 
     } else {
 
         options.speechText = "Incorrect usage.To start a conference, please provide a telephone number or a Be Anywhere device.";
         options.cardContent = "Incorrect usage.  To start a conference, please provide a telephone number or a Be Anywhere device.";
-        options.imageUrl = "https://cdn.pixabay.com/photo/2012/04/12/22/25/warning-sign-30915_1280.png";
-
+        options.imageObj = phoneErrorImg;
+        options.cardTitle = "ERROR: Audio Conference Start";
     }
-
-    options.cardTitle = "Audio Conference Start";
 
     options.endSession = true;
 
@@ -181,39 +216,43 @@ function handleStopIntent(request,context,session) {
             //stopConference(phone_number)
             options.speechText = `Your conference was stopped on <say-as interpret-as="telephone">${request.intent.slots.PN.value}</say-as>.`;
             options.cardContent = `Your conference was stopped on ${request.intent.slots.PN.value}.`;
-            options.imageUrl = "https://www.pngimg.com/uploads/phone/phone_PNG48981.png";
+            options.imageObj = phoneStopImg;
+            options.cardTitle = "Audio Conference Stop";
         //If a device was not provided
         } else if (request.intent.slots.BeAnywhere.value) {
             //stop conference on the BeAnywhere device that was provided
             //stopConference(be_anywhere)
             options.speechText = `Your conference was stopped on ${request.intent.slots.BeAnywhere.value}.`;
             options.cardContent = `Your conference was stopped on ${request.intent.slots.BeAnywhere.value}.`;
-            options.imageUrl = "https://www.pngimg.com/uploads/phone/phone_PNG48981.png";
+            options.imageObj = phoneStopImg;
+            options.cardTitle = "Audio Conference Stop";
         } else if (PN_GLOBAL != "") {
             //stop conference on the phone number that the conference was started on
             //stopConference(phone_number)
             options.speechText = `Your conference was stopped on <say-as interpret-as="telephone">${PN_GLOBAL}</say-as>.`;
             options.cardContent = `Your conference was stopped on ${PN_GLOBAL}.`;
-            options.imageUrl = "https://www.pngimg.com/uploads/phone/phone_PNG48981.png";
+            options.imageObj = phoneStopImg;
+            options.cardTitle = "Audio Conference Stop";
         } else if (BeAnywhere_GLOBAL != "") {
             //stop conference on the BeAnywhere device that the conference was started on
             //stopConference(be_anywhere)
             options.speechText = `Your conference was stopped on ${BeAnywhere_GLOBAL}.`;
             options.cardContent = `Your conference was stopped on ${BeAnywhere_GLOBAL}.`;
-            options.imageUrl = "https://www.pngimg.com/uploads/phone/phone_PNG48981.png";
+            options.imageObj = phoneStopImg;
+            options.cardTitle = "Audio Conference Stop";
         } else {
             options.speechText = "Invalid option.To stop the conference, please provide a valid telephone number or BeAnywhere device.";
             options.cardContent = "Invalid option.  To stop the conference, please provide a valid telephone number or BeAnywhere device.";
-            options.imageUrl = "https://cdn.pixabay.com/photo/2012/04/12/22/25/warning-sign-30915_1280.png"
+            options.imageObj = phoneErrorImg;
+            options.cardTitle = "ERROR: Audio Conference Stop";
         }
 
     } else {
         options.speechText = "Incorrect usage.To stop a conference, a conference must first be started.";
         options.cardContent = "Incorrect usage.  To stop a conference, a conference must first be started.";
-        options.imageUrl = "https://cdn.pixabay.com/photo/2012/04/12/22/25/warning-sign-30915_1280.png"
+        options.imageObj = phoneErrorImg;
+        options.cardTitle = "ERROR: Audio Conference Stop";
     }
-
-    options.cardTitle = "Audio Conference Stop";
 
     options.endSession = true;
 
