041d4075029d0d53cac79c26237db24891bcc395
Ricky Mangerie <ricky@drexel.edu>
2018-11-07 04:55:11 -0500

    Created structure for GoLang lambda

--- lambdaFun/GoLang/main.go
+++ lambdaFun/GoLang/main.go
@@ -1,106 +1,36 @@
 package main
 
 import (
-	//AWS Lambda Context Object in Go
-	"context"
-	"fmt"
-	"log"
+	"testGoLang/alexa"
 
-	//Implements the Lambda programming model for Go
-	"github.com/aws/aws-lambda-go/lambda" //go get -u github.com/aws/aws-lambda-go/lambda
-
-	//Implements a deep pretty printer for Go data structures to aid in debugging
-	"github.com/davecgh/go-spew/spew" //go get -u github.com/davecgh/go-spew/spew
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-//Input object
-type AlexaRequest struct {
-	//string "Version": skill version number
-	Version string `json:"version"`
-	//object "Request": current request data
-	Request struct {
-		//string "Type": current request type
-		Type string `json:"type"`
-		//string "Time": current request UTC time
-		Time string `json:"timestamp"`
-		//object "intent": current intent data
-		Intent struct {
-			//string "Name": current intent name
-			Name string `json:"name"`
-			//string "ConfirmationStatus": if the user needs to provide confirmation upon invocation of the current intent
-			ConfirmationStatus string `json:"confirmationstatus"`
-		} `json:"intent"`
-	} `json:"request"`
-}
-
-//Output object
-type AlexaResponse struct {
-	//string "Version": skill version number
-	Version string `json:"version"`
-	//object "Response": current response data
-	Response struct {
-		//object "OutputSpeech": current output speech data
-		OutputSpeech struct {
-			//string "Type": current data type of the output speech
-			Type string `json:"type"`
-			//string "Text": current text of the output speech
-			Text string `json:"text"`
-		} `json:"outputSpeech"`
-	} `json:"response"`
-}
-
-func CreateResponse() *AlexaResponse {
-	var resp AlexaResponse
-	resp.Version = "1.0"
-	resp.Response.OutputSpeech.Type = "PlainText"
-	resp.Response.OutputSpeech.Text = "Hello.  Please override this default output."
-	return &resp
-}
-
-func (resp *AlexaResponse) Say(text string) {
-	resp.Response.OutputSpeech.Text = text
-}
-
-/*
-	name: HandleRequest()
-	description: The Lambda handler signature, includes the code which will be executed
-	parameters:
-		ctx context.Context: Provides runtime information for your Lambda function invocation.
-			ctx: The variable declared to leverage the information avaiable via AWS Lambda Context Object in Go
-		i AlexaRequest: The input request
-	return:
-		AlexaResponse: The output response
-		error: Returns standard error information
-*/
-func HandleRequest(ctx context.Context, i AlexaRequest) (AlexaResponse, error) {
-	// Use Spew to output the request for debugging purposes:
-	fmt.Println("---- Dumping Input Map: ----")
-	spew.Dump(i)
-	fmt.Println("---- Done. ----")
-
-	// Example of accessing map value via index:
-	log.Printf("Request type is ", i.Request.Intent.Name)
-
-	// Create a response object
-	resp := CreateResponse()
-
-	// Customize the response for each Alexa Intent
-	switch i.Request.Intent.Name {
-	case "officetemp":
-		resp.Say("The current temperature is 68 degrees.")
-	case "hello":
-		resp.Say("Hello there, Lambda appears to be working properly.")
-	case "AMAZON.HelpIntent":
-		resp.Say("This app is easy to use, just say: ask the office how warm it is")
-	default:
-		resp.Say("I'm sorry, the input does not look like something I understand.")
-	}
-
-	return *resp, nil
+func Handler(request alexa.Request) (alexa.Response, error) {
+	return IntentDispatcher(request), nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(Handler)
+}
+
+func HandleStartIntent(request alexa.Request) alexa.Response {
+	//slots := request.Body.Intent.Slots
+	var builder alexa.SSMLBuilder
+	builder.Say("Here are the current frontpage deals:")
+	builder.Pause("1000")
+	return alexa.NewSSMLResponse("Frontpage Deals", builder.Build())
+}
+
+func IntentDispatcher(request alexa.Request) alexa.Response {
+	var response alexa.Response
+	switch request.Body.Intent.Name {
+	case "StartIntent":
+		response = HandleStartIntent(request)
+	default:
+		response = HandleStartIntent(request)
+	}
+	return response
 }
 
 /*
